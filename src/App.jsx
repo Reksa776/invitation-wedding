096a576be9cd82720165b841c567cee89e91ba7c
Reksa776 <116544285+Reksa776@users.noreply.github.com>
2025-09-14 18:01:34 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import WeddingPage from "./pages/WeddingPage";
-import InputUrl from "./pages/InputUrl";
 import Login from "./pages/Login";
+import RSVPPage from "./pages/RSVPPage";
 
 function App() {
   const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route index path="/" element={<WeddingPage />} />
-          <Route index path="/live-streaming" element={isLoggedIn ? <InputUrl /> : <Login />} />
+          <Route index path="/live-streaming" element={isLoggedIn ? <RSVPPage /> : <Login />} />
 
         </Routes>
       </Router>
